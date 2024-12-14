1db9bf45f59e1246ca6cf47d87e9db24ec44495b
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-12-14 12:21:14 +0530

    add hello rest controller

---
+++ src/main/java/com/web_app/springboot/demo/my_spring_boot_app/myapp/FunRestController.java
@@ -0,0 +1,13 @@
+package com.web_app.springboot.demo.my_spring_boot_app.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello, this is abinesh";
+    }
+}
