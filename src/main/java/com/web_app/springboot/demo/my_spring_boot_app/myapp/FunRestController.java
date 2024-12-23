1121c828bacb51ca6ba40f653b7bf67e6951346e
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-12-23 09:01:37 +0530

    inject application properties to code

--- src/main/java/com/web_app/springboot/demo/my_spring_boot_app/myapp/FunRestController.java
+++ src/main/java/com/web_app/springboot/demo/my_spring_boot_app/myapp/FunRestController.java
@@ -1,10 +1,17 @@
 package com.web_app.springboot.demo.my_spring_boot_app.myapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello(){
@@ -14,5 +21,10 @@ public class FunRestController {
     @GetMapping("/workout")
     public String workout(){
         return "Going to gym for workout...";
+    }
+
+    @GetMapping("/myteaminfo")
+    public String myteaminfo(){
+        return "coachName: "+coachName+" teamName: "+teamName;
     }
 }
