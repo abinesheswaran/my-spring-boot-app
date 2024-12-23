9d92f48d9e010cfcf1c6faaef3556333235d2dd4
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-12-23 15:54:56 +0530

    add lazy initialization

--- src/main/java/com/web_app/springboot/demo/my_spring_boot_app/myapp/utils/TrackCoach.java
+++ src/main/java/com/web_app/springboot/demo/my_spring_boot_app/myapp/utils/TrackCoach.java
@@ -1,9 +1,15 @@
 package com.web_app.springboot.demo.my_spring_boot_app.myapp.utils;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TrackCoach implements Coach{
+@Lazy
+public class TrackCoach implements Coach {
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String dailyWorkout() {
         return "Run a hard 5k!";
