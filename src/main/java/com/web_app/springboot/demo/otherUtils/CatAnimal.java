dab309fb522720ee808108c333927ff2ab9e5b81
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-12-23 22:04:21 +0530

    add bean lifecycle

--- src/main/java/com/web_app/springboot/demo/otherUtils/CatAnimal.java
+++ src/main/java/com/web_app/springboot/demo/otherUtils/CatAnimal.java
@@ -1,14 +1,30 @@
 package com.web_app.springboot.demo.otherUtils;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //this is default one
-//@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class CatAnimal implements Animal{
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class CatAnimal implements Animal {
+
+    public CatAnimal() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
 
     @Override
     public String move() {
