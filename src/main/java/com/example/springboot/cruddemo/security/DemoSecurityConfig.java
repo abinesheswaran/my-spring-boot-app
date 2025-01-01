1cd71f199d3e4322901b9a9b9d67aba900470cda
HQ\abe <abinesh.e@kudelskisecurity.com>
2025-01-01 11:11:36 +0530

    add authorization details to db

--- src/main/java/com/example/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/example/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,21 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("John").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("Mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder().username("Susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        //if everything is already in right name...
+        //return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails john = User.builder().username("John").password("{noop}test123").roles("EMPLOYEE").build();
+//        UserDetails mary = User.builder().username("Mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+//        UserDetails susan = User.builder().username("Susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
